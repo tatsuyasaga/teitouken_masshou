234ff0bbb7308f69a82ad2d526ef66e4d2d028b3
tatsuyasaga <t.saga@yahoo.ne.jp>
2021-03-29 19:13:34 +0900

    userのfactorybotを修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname {"abc"}
     name {"佐藤太郎"}
-    postal_code {"810-0001"}
+    postal_code {"8100001"}
     address {"１丁目１番１号"}
     phone_number {"09000000000"}
     email {"a@a"}
