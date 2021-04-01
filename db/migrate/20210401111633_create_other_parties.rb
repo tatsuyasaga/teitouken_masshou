700c44b1ec62454d0ca67d24803eee9b3c31ccbe
tatsuyasaga <t.saga@yahoo.ne.jp>
2021-04-01 20:57:02 +0900

    other_partyのテーブルを作成

--- db/migrate/20210401111633_create_other_parties.rb
+++ db/migrate/20210401111633_create_other_parties.rb
@@ -1,7 +1,11 @@
 class CreateOtherParties < ActiveRecord::Migration[6.0]
   def change
     create_table :other_parties do |t|
-
+      t.string :address,               null: false
+      t.string :name,                  null: false
+      t.string :ceo,                   null: false
+      t.integer :number,               null: false
+      t.references :application_datum, foreign_key: true
       t.timestamps
     end
   end
