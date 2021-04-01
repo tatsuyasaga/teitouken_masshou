0b9e7ea7348c301b826b3d5751d9e5655310ed5f
tatsuyasaga <t.saga@yahoo.ne.jp>
2021-04-01 22:52:29 +0900

    landモデルを編集

--- app/models/land.rb
+++ app/models/land.rb
@@ -1,2 +1,8 @@
 class Land < ApplicationRecord
+  with_options presence: true do
+    validates :prefecture_id, numericality: {other_than: 1, message: "Select"}
+    validates :city
+    validates :number
+    validates :type_id, numericality: {other_than: 1, message: "Select"}
+    validates :acreage
 end
