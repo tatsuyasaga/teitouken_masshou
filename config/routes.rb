919f089b828879f2ec2a706f084279b83a0ae26b
tatsuyasaga <t.saga@yahoo.ne.jp>
2021-04-01 18:58:27 +0900

    ルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,4 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "application_data#index"
   resources :application_data, only: [:index, :new, :create]
-  root to: "application_data#index"
-  resources :application_data, only: [:index]
 end
