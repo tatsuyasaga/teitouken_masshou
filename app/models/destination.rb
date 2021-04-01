dad115ce596187ec685351f22ca60a1749c088b4
tatsuyasaga <t.saga@yahoo.ne.jp>
2021-04-01 22:53:33 +0900

    destinationのid1のnameを修正

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,6 +1,6 @@
 class Destination < ActiveHash::Base
   self.data = [
-    { id: 1, name: "--" },
+    { id: 1, name: "管轄の法務局を選択" },
     { id: 2, name: "札幌法務局" },
     { id: 3, name: "函館地方法務局" },
     { id: 4, name: "旭川地方法務局" },
