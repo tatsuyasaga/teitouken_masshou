52ce6f452fa888c0508ca4437338487e99c6b0eb
tatsuyasaga <t.saga@yahoo.ne.jp>
2021-04-01 22:52:58 +0900

    application_datumモデルのバリデーションを修正

--- app/models/application_datum.rb
+++ app/models/application_datum.rb
@@ -3,7 +3,7 @@ class ApplicationDatum < ApplicationRecord
     validates :reason_date
     validates :reason
     validates :receipt_number
-    validates :destination_id
+    validates :destination_id, numericality: {other_than: 1, message: "Select"}
     validates :tax
   end
 
