33a18bec58bcab4f2ceb9e1b52d809eb70f818cb
tatsuyasaga <t.saga@yahoo.ne.jp>
2021-04-14 01:21:26 +0900

    運営者情報ページへのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "application_data#index"
   resources :application_data, only: [:index, :new, :create, :destroy]
+  resources :application_data do
+    collection do
+      get "operator_info"
+    end
+  end
 end
