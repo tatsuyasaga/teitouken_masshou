7736e6dbcc0bc84ef296e5acaaab419373c64f4f
tatsuyasaga <t.saga@yahoo.ne.jp>
2021-04-28 23:17:51 +0900

    支局・出張所のカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_01_150218) do
+ActiveRecord::Schema.define(version: 2021_04_28_135321) do
 
   create_table "application_data", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.date "reason_date", null: false
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_04_01_150218) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "branch_destination_id"
     t.index ["user_id"], name: "index_application_data_on_user_id"
   end
 
